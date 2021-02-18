8a6a80ffda72e08014eced3467bf168a5a457e71
AldeiaCloud <78882441+aldeiacloud@users.noreply.github.com>
2021-02-18 19:24:36 -0300

    Update configure_vsftpd.sh

--- configure_vsftpd.sh
+++ configure_vsftpd.sh
@@ -1,6 +1,6 @@
 cd /tmp
-apt update
-apt install vsftpd -y
+sudo apt update
+sudo apt install vsftpd -y
 sudo wget https://raw.githubusercontent.com/aldeiacloud/config_vsftpd/main/vsftpd.conf
 sudo mv vsftpd.conf /etc/vsftpd.conf
 sudo systemctl enable vsftpd
